c8e8b986a58ab3ab82c3e5ce928654cbe0e1e1a0
Ahmed anbar <begnulinux@gmail.com>
2026-04-23 09:50:22 +0300

    test: trigger devguard PR run

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function devguardSmokeTest(): mixed
+    {
+        return env('TOTALLY_FAKE_KEY');
+    }
 }
+// trigger
